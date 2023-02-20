227fa9663e717717e92070af0d4fc8e3a8afc922
Your Name <you@example.com>
2023-02-20 19:06:29 +0300

    commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div className="nav">
+        <a href="">Home</a>
+        <a href="">about</a>
+        <a href="">contact</a>
+        <a href="">services</a>
+    </div>
+  )
+}
+
+export default header
